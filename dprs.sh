f781546b5a26ba68e7d78a864610ce15b8e4e828
sec <bmg8551@naver.com>
2021-10-29 18:07:30 +0900

    dprs basic

--- dprs.sh
+++ dprs.sh
@@ -6,14 +6,15 @@
 # Powered by Seculayer © 2021 Service Model Team, R&D Center.
 ######################################################################################
 
-APP_PATH=/eyeCloudAI/app/ape
-
-DPRS_LIB_PATH=$APP_PATH/dprs/lib
-####
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-export CUDA_HOME=/usr/local/cuda
-export PYTHONPATH=$DPRS_LIB_PATH
-
-KEY=${1}
-
-/usr/local/bin/python3.7 -m mars.DataProcessRecommender ${KEY}
+#APP_PATH=/eyeCloudAI/app/ape
+#
+#DPRS_LIB_PATH=$APP_PATH/dprs/lib
+#####
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+#export CUDA_HOME=/usr/local/cuda
+#export PYTHONPATH=$DPRS_LIB_PATH
+#
+#KEY=${1}
+#
+#/usr/local/bin/python3.7 -m mars.DataProcessRecommender ${KEY}
+echo "hello dprs"
