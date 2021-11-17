b538a8ac4db2e851f588b769a875f2a57880d1d2
Wonjoon Lee <wonjoon.lee@seculayer.com>
2021-11-17 16:48:28 +0900

    LongToIP, 테스트 추가

---
+++ dprs/recommender/dp/LongToIP.py
@@ -0,0 +1,14 @@
+import logging
+from typing import List
+
+from dprs.recommender.dp.AbstractConvert import AbstractConvert
+
+
+class LongToIP(AbstractConvert):
+    def apply(self, data: int) -> List[str]:
+        try:
+            # TODO: add validation input range
+            return [".".join(str(data >> (i << 3) & 0xFF) for i in reversed(range(4)))]
+        except Exception as e:
+            logging.error(e)
+            return [0]  # ?
