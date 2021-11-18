7ac0fcd9562bce47447d266ab15d5618de71239e
Wonjoon Lee <wonjoon.lee@seculayer.com>
2021-11-18 15:15:15 +0900

    RegexGet 테스트 추가

--- tests/recommender/dp/test_regex_get.py
+++ tests/recommender/dp/test_regex_get.py
@@ -11,3 +11,9 @@ def test_regex_get_two_group():
     regex = RegexGet(arg_list=[r"[a-z]+_(\d+)_[a-z]+_(\d+)"])
 
     assert regex.apply("prefix_123_suffix_213") == ["123"]
+
+
+def test_regex_get_no_group():
+    regex = RegexGet(arg_list=[r"[a-z]"])
+
+    assert regex.apply("prefix_123_suffix_213") == [""]
