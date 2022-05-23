23f1d19d5a84fa69d2b7a61381507cb99d173a54
이원준 <wonjoon.lee@seculayer.com>
2022-05-23 11:24:07 +0900

    사용하지 않는 test 제거

--- tests/recommender/dp/test_min_max_normal.py
+++
@@ -1,8 +0,0 @@
-from dprs.recommender.dp.MinMaxNormal import MinMaxNormal
-
-
-def test_minmax_8bit():
-    minmax = MinMaxNormal(stat_dict={"min": 0, "max": 255})
-    assert minmax.apply(255) == [1.0]
-    assert minmax.apply(0) == [0.0]
-    assert minmax.apply(128) == [(128 / 255)]
--- tests/recommender/dp/test_substr.py
+++
@@ -1,8 +0,0 @@
-from dprs.recommender.dp.Substr import Substr
-
-
-def test_substr():
-    sub = Substr(arg_list=[0, 1])
-
-    assert sub.apply("Korea") == ["K"]
-    assert sub.apply(123) == [""]
