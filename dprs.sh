3116965c02607c1007017ee6e7ce26043c198a47
이원준 <wonjoon.lee@seculayer.com>
2022-08-25 13:33:32 +0900

    `dprs.sh`에서 PYTHON_BIN 설정 오류 수정

--- dprs.sh
+++ dprs.sh
@@ -9,11 +9,10 @@
 APP_PATH=/eyeCloudAI/app/ape
 
 
-if [ -x "${APP_PATH}/dprs/.venv/bin/python3" ]
-then
-  $PYTHON_BIN="$APP_PATH/dprs/.venv/bin/python3"
+if [ -x "${APP_PATH}/dprs/.venv/bin/python3" ]; then
+  PYTHON_BIN="${APP_PATH}/dprs/.venv/bin/python3"
 else
-  $PYTHON_BIN="$(command -v python3)"
+  PYTHON_BIN="$(command -v python3)"
 fi
 
 KEY=${1}
