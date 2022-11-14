d8682f5ca6d0518fa2ae4d214ebb73f4edb839d9
Manki Baek <manki.baek@seculayer.com>
2022-11-14 18:29:30 +0900

    오류 수정

--- dprs.sh
+++ dprs.sh
@@ -12,7 +12,7 @@ APP_PATH=/eyeCloudAI/app/ape
 if [ -x "${APP_PATH}/dprs/.venv/bin/python3" ]; then
   PYTHON_BIN="${APP_PATH}/dprs/.venv/bin/python3"
 else
-  PYTHON_BIN="$(command -v python3)"
+  PYTHON_BIN="$(command -v python3.7)"
   export PYTHONPATH=$PYTHONPATH:$APP_PATH/dprs/lib:$APP_PATH/dprs
   export PYTHONPATH=$PYTHONPATH:$APP_PATH/pycmmn/lib:$APP_PATH/pycmmn
   export PYTHONPATH=$PYTHONPATH:$APP_PATH/dataconverter/lib:$APP_PATH/dataconverter
